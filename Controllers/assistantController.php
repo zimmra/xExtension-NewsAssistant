ccd380f8695ca4e8b3c0cb6bd8a50ff42d5432b2
zimmra <paytonz@icloud.com>
2023-12-13 09:47:49 -0800

    Add and import markedjs

--- Controllers/assistantController.php
+++ Controllers/assistantController.php
@@ -42,7 +42,8 @@ class FreshExtension_assistant_Controller extends Minz_ActionController
 	public function summaryAction()
 	{
 		Minz_View::appendStyle($this->getFileUrl('style.css', 'css'));
-		Minz_View::appendScript($this->getFileUrl('script.js', 'js'));
+		Minz_View::prependScript($this->getFileUrl('marked.min.js', 'js'), false, true, false);
+		Minz_View::appendScript($this->getFileUrl('script.js', 'js'), false, true, false);
 	}
 
 	private function _echoData(string $data, string $event_name = '')
